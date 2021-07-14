a65f64cd00ba393c13ac31b4b4b22082de09f1a3
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-07-15 00:20:03 +0300

    made footer responsive.

--- src/components/footer/FooterElements.js
+++ src/components/footer/FooterElements.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import Colors from '../../Colors';
+import Breakpoints from '../../Breakpoints';
 
 export const Container = styled.div`
   background: ${Colors.darkModerateCyan};
@@ -8,10 +9,17 @@ export const Container = styled.div`
   align-items: center;
   padding: 2.5em;
   font-size: 1.5vw;
+
+  @media (max-width: ${Breakpoints.sm}) {
+    padding: 10em 5em;
+  }
 `;
 
 export const LogoContainer = styled.div`
   margin-bottom: 2em;
+  @media (max-width: ${Breakpoints.sm}) {
+    margin-bottom: 4em;
+  }
 `;
 
 export const Links = styled.div`
@@ -19,6 +27,10 @@ export const Links = styled.div`
   color: ${Colors.darkDesaturatedCyan};
   text-transform: capitalize;
   margin-bottom: 4em;
+
+  @media (max-width: ${Breakpoints.sm}) {
+    font-size: 4vw;
+  }
 `;
 
 export const Socials = styled.div`
