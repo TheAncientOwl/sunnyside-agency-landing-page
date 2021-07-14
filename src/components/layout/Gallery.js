cd87c32db054f0988e7bfb0804b18345e90047f2
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-07-14 22:29:32 +0300

    implemented gallery.

--- src/components/layout/Gallery.js
+++ src/components/layout/Gallery.js
@@ -1,12 +1,21 @@
+import { ImageGalleryCone, ImageGalleryMilkbottles, ImageGalleryOrange, ImageGallerySugarcubes } from '../Images';
 import { Grid, ImageBox } from './LayoutElements';
 
 export default function Gallery() {
   return (
     <Grid double>
-      <ImageBox></ImageBox>
-      <ImageBox></ImageBox>
-      <ImageBox></ImageBox>
-      <ImageBox></ImageBox>
+      <ImageBox>
+        <ImageGalleryMilkbottles />
+      </ImageBox>
+      <ImageBox>
+        <ImageGalleryOrange />
+      </ImageBox>
+      <ImageBox>
+        <ImageGalleryCone />
+      </ImageBox>
+      <ImageBox>
+        <ImageGallerySugarcubes />
+      </ImageBox>
     </Grid>
   );
 }
