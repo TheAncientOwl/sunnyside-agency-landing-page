a56ef14517718e485aa5436734b893b7017922b6
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-07-14 19:38:05 +0300

    added colors.

---
+++ src/Colors.js
@@ -0,0 +1,16 @@
+const Colors = {
+  softRed: 'hsl(7, 99%, 70%)',
+  yellow: 'hsl(51, 100%, 49%)',
+  darkDesaturatedCyan: 'hsl(167, 40%, 24%)',
+  darkBlue: 'hsl(198, 62%, 26%)',
+  darkModerateCyan: 'hsl(168, 34%, 41%)',
+
+  darkDesaturatedBlue: 'hsl(212, 27%, 19%)',
+  darkGrayBlue: 'hsl(213, 9%, 39%)',
+  darkGrayBlue2: 'hsl(232, 10%, 55%)',
+  grayBlue: 'hsl(210, 4%, 67%)',
+  white: 'hsl(0, 0%, 100%)',
+};
+
+Object.freeze(Colors);
+export default Colors;
